523736b4f1211e13f61a40762047f8383a842d8f
Neal Zheng <Qinjian.Zheng@recordpoint.com>
2023-02-10 15:45:52 +1100

    init commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (args.Length == 0)
+{
+  Console.WriteLine("Hello");
+}
+else
+{
+  Console.WriteLine($"Hello, {args[0]}");
+}
+
+
